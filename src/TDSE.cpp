0d470759dc361ec677a9fff2e828bef550a17a8a
Joel-Venzke <joel.venzke@drake.edu>
2016-11-21 21:26:43 -0700

    clean up old test code

--- src/TDSE.cpp
+++ src/TDSE.cpp
@@ -18,26 +18,5 @@ int main() {
     Simulation s;
     Wavefuction w;
 
-    // std::cout << "Hello\n";
-    // json j;
-    // j["array_test"] = {2,3,4};
-    // j["arrays"]["pulse_1"] = {2,3,4};
-    // j["arrays"]["pulse_2"] = {2,3,4};
-    // j["arrays"]["pulse_2"].push_back({3,2,3,4,5});
-    
-    // int a[3] = {2,3,4};
-    // for ( int i=0; i<3; i++ ){
-    //     std::cout << a[i] << "\n";
-    //     j["arrays"]["pulse_2"].push_back(a[i]);
-    // }
-
-    // std::cout << j.dump() << std::endl;
-    // std::cout << j["arrays"]["pulse_2"][2] << std::endl;
-
-    // std::cout << file_to_string("file.txt") << "\n";
-
-    // json j2 = file_to_string("file.txt");
-    // std::string s = j2.dump(4);
-    // std::cout << s << "\n";
     return 0;
 }
