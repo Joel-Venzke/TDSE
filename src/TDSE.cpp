1ec235bfd78df51cf14ab2d86b1b92c0b4c2d128
Joel-Venzke <joel.venzke@drake.edu>
2017-03-28 13:18:19 -0600

    tabs

--- src/TDSE.cpp
+++ src/TDSE.cpp
@@ -12,37 +12,37 @@
 
 int main(int argc, char** argv)
 {
-    PETSCWrapper p_wrap(argc,argv);
-    p_wrap.PushStage("Set up");
+  PETSCWrapper p_wrap(argc,argv);
+  p_wrap.PushStage("Set up");
 
-    /* initialize all of the classes */
-    ViewWrapper viewer_file("TDSE.h5");
-    Parameters parameters(viewer_file, "input.json");
-    HDF5Wrapper data_file(parameters);
-    Pulse pulse(data_file, parameters);
-    // Wavefunction wavefunction(data_file,parameters);
-    // Hamiltonian hamiltonian(wavefunction,pulse,data_file,parameters);
-    // Simulation s(hamiltonian,wavefunction,pulse,data_file,parameters);
-    p_wrap.PopStage();
+  /* initialize all of the classes */
+  ViewWrapper viewer_file("TDSE.h5");
+  Parameters parameters(viewer_file, "input.json");
+  HDF5Wrapper data_file(parameters);
+  Pulse pulse(data_file, parameters);
+  // Wavefunction wavefunction(data_file,parameters);
+  // Hamiltonian hamiltonian(wavefunction,pulse,data_file,parameters);
+  // Simulation s(hamiltonian,wavefunction,pulse,data_file,parameters);
+  p_wrap.PopStage();
 
-    p_wrap.PushStage("Eigen State");
-    /* get ground states */
-    // switch (parameters.get_state_solver_idx()) {
-    //     case 0: /* File */
-    //         break;
-    //     case 1: /* ITP */
-    //         s.imag_time_prop(parameters.get_num_states());
-    //         break;
-    //     case 2: /* Power */
-    //         s.power_method(parameters.get_num_states());
-    //         break;
-    // }
-    p_wrap.PopStage();
+  p_wrap.PushStage("Eigen State");
+  /* get ground states */
+  switch (parameters.get_state_solver_idx()) {
+    case 0: /* File */
+      break;
+    case 1: /* ITP */
+      s.imag_time_prop(parameters.get_num_states());
+      break;
+    case 2: /* Power */
+      s.power_method(parameters.get_num_states());
+      break;
+  }
+  p_wrap.PopStage();
 
-    p_wrap.PushStage("Propagation");
-    // if (parameters.get_propagate()==1) {
-    //     s.propagate();
-    // }
-    p_wrap.PopStage();
-    return 0;
+  p_wrap.PushStage("Propagation");
+  if (parameters.get_propagate()==1) {
+    s.propagate();
+  }
+  p_wrap.PopStage();
+  return 0;
 }
