ad0be57e1f3f9728db328edb0b05cf0d7a479be8
Joel-Venzke <joel.venzke@drake.edu>
2017-05-19 16:41:51 -0600

    fix the SAE pot to support multiple nuclie

--- src/Hamiltonian.cpp
+++ src/Hamiltonian.cpp
@@ -364,13 +364,13 @@ dcomp Hamiltonian::GetNucleiTerm(std::vector<int>& idx_array)
       else /* SAE */
       {
         r = SoftCoreDistance(location[nuclei_idx], idx_array, elec_idx);
-        nuclei -= dcomp(c0[nuclei_idx], 0.0);
-        nuclei -= dcomp(z_c[nuclei_idx] * exp(-r0[nuclei_idx] * r), 0.0);
+        nuclei -= dcomp(c0[nuclei_idx] / r, 0.0);
+        nuclei -= dcomp(z_c[nuclei_idx] * exp(-r0[nuclei_idx] * r) / r, 0.0);
         for (int i = 0; i < sae_size[nuclei_idx]; ++i)
         {
-          nuclei -= dcomp(a[nuclei_idx][i] * exp(-b[nuclei_idx][i] * r), 0.0);
+          nuclei -=
+              dcomp(a[nuclei_idx][i] * exp(-b[nuclei_idx][i] * r) / r, 0.0);
         }
-        nuclei /= r;
       }
     }
   }
