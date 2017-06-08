2f967e2cec848a5547bdfbf9a43be7e84a9a0720
Joel-Venzke <joel.venzke@drake.edu>
2017-06-08 14:44:19 -0600

    call split opp

--- src/TDSE.cpp
+++ src/TDSE.cpp
@@ -52,7 +52,8 @@ int main(int argc, char** argv)
   {
     p_wrap.Print(
         "\n************************ Propagation ************************\n\n");
-    s.Propagate();
+    // s.Propagate();
+    s.SplitOpperator();
   }
   p_wrap.PopStage(); /* Propagation */
 
