67f90ee5670a3e4b52aa1920c87dec94b68ce48f
Joel-Venzke <joel.venzke@drake.edu>
2017-07-11 17:37:02 -0600

    some higher order issue

--- src/Hamiltonian.cpp
+++ src/Hamiltonian.cpp
@@ -245,16 +245,17 @@ void Hamiltonian::SetUpCoefficients()
     radial_bc_coef.resize(order / 2,
                           std::vector< std::vector< dcomp > >(
                               3, std::vector< dcomp >(order + 1, 0.0)));
-    /* Set up real gird for 1st and 2nd order derivatives */
-    // for (int coef_idx = 0; coef_idx < order + 1; ++coef_idx)
+    // std::vector< dcomp > x_vals_bc(order + 2, 0.0);
+    // /* Set up real gird for 1st and 2nd order derivatives */
+    // for (int coef_idx = 0; coef_idx < order + 2; ++coef_idx)
     // {
-    //   x_vals[coef_idx] = delta_x[0] * coef_idx;
+    //   x_vals_bc[coef_idx] = delta_x[0] * coef_idx;
     // }
     // for (int discontinuity_idx = 0; discontinuity_idx < order / 2;
     //      ++discontinuity_idx)
     // {
     //   /* Get real coefficients for 2nd derivative (order+2 terms) */
-    //   FDWeights(x_vals, 2, radial_bc_coef[discontinuity_idx],
+    //   FDWeights(x_vals_bc, 2, radial_bc_coef[discontinuity_idx],
     //             discontinuity_idx);
     // }
     for (int discontinuity_idx = 0; discontinuity_idx < order / 2;
@@ -331,8 +332,8 @@ dcomp Hamiltonian::GetVal(PetscInt idx_i, PetscInt idx_j, bool time_dep,
       return GetOffDiagonal(idx_array, diff_array, time_dep, time_idx,
                             only_dim_idx, ecs);
     }
-    // else if (coordinate_system_idx == 1 and diff_array[0] > 0 and
-    //          sum < order + 1)
+    // else if (coordinate_system_idx == 1 and diff_array[0] != 0 and
+    //          sum < order + 2)
     // {
     //   return GetOffDiagonal(idx_array, diff_array, time_dep, time_idx,
     //                         only_dim_idx, ecs);
