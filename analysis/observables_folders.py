8fba176fe4bc137ec270f8f7660a9db3cb9664c7
Joel-Venzke <joel.venzke@drake.edu>
2017-07-25 11:59:35 -0600

    debug statement

--- analysis/observables_folders.py
+++ analysis/observables_folders.py
@@ -229,6 +229,8 @@ for fold in folders:
                 padd2 = 2**np.ceil(np.log2(data.shape[0]) * 4)
                 paddT = np.max(time) * padd2 / data.shape[0]
                 dH = 2 * np.pi / paddT / energy
+                print int(np.floor((padd2 - data.shape[0]) / 2)), int(
+                    np.ceil((padd2 - data.shape[0]) / 2))
                 if np.max(data) > 1e-19:
                     data = np.absolute(
                         np.fft.fft(
