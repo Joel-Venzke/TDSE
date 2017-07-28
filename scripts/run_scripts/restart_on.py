1ac7f929087b369cc65774740115b5c9b8aab64a
Joel-Venzke <joel.venzke@drake.edu>
2017-07-28 11:07:42 -0600

    add comments

--- scripts/run_scripts/restart_on.py
+++ scripts/run_scripts/restart_on.py
@@ -1,9 +1,12 @@
 import json
 
+# read current input file
 with open('input.json', 'r') as data_file:
     data = json.load(data_file)
 
+# turn on restart
 data["restart"] = 1
 
+# write file
 with open('input.json', 'w') as data_file:
     data_file.write(json.dumps(data, indent=1) + "\n")
