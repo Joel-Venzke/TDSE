fixed field max calculation and some plot updates

analysis/2d_fft.py
@@ -51,7 +51,7 @@ for i, psi in enumerate(psi_value):
                     # np.abs(psi),
                     cmap='viridis',
                     origin='lower',
-                    norm=LogNorm(vmin=1e-5),
+                    # norm=LogNorm(vmin=1e-5),
                     extent=[
                         ky.min(),
                         ky.max(), -1.0 * kx.max() / 2.0,

analysis/get_w.py
@@ -20,11 +20,11 @@ w_min = w_0 / 2
 w_max = w_0 * 2
 dt = 0.045
 plot_data = []
-for cycles in range(10, 11):
+for cycles in range(1, 10):
     w_min = w_0 / 2
     w_max = w_0 * 2
     while (np.abs(w_max - w_min) > 1e-6):
-        print(w_max + w_min) / 2.0, " Error: ", np.abs(w_max - w_min)
+        # print(w_max + w_min) / 2.0, " Error: ", np.abs(w_max - w_min)
         time, pulse = get_pulse(cycles / 2.0, cycles / 2.0, 5.31e13,
                                 (w_max + w_min) / 2.0, dt)
 
@@ -44,7 +44,7 @@ for cycles in range(10, 11):
         else:
             w_min = (w_max + w_min) / 2.0
     plot_data.append((w_max + w_min) / 2.0)
-    print cycles, plot_data[-1]
+    print cycles, plot_data[-1], (w_0 - plot_data[-1]) / w_0
 print plot_data
 
 plt.plot(range(1, 5), plot_data, 'r-o', label="$\omega_A$")

analysis/pulses.py
@@ -12,6 +12,7 @@ num_dims = f["Parameters"]["num_dims"][0]
 num_electrons = f["Parameters"]["num_electrons"][0]
 num_pulses = f["Parameters"]["num_pulses"][0]
 checkpoint_frequency = f["Parameters"]["write_frequency_observables"][0]
+energy = f["Parameters"]["energy"][0]
 
 # Field Plot
 print "Plotting A Field"
@@ -99,10 +100,12 @@ for dim_idx in range(num_dims):
         plt.semilogy(spec_time, data_fft, label="field " + str(dim_idx))
         grid_max = max(spec_time[np.argmax(data_fft[:data_fft.shape[0] / 2])],
                        grid_max)
-
+plt.axvline(x=energy, color='k')
+plt.axvline(x=grid_max, color='r')
 plt.ylabel("Field Spectrum (arb)")
 plt.xlabel("$\omega$ (a.u.)")
 plt.title("Field Spectrum")
 plt.xlim([0, grid_max * 3.0])
 plt.legend()
 fig.savefig("figs/Spectrum.png")
+print "Omega Error (A vs E):", grid_max - energy