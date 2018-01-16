a8697522044896579d76536e46189494e4d7cc02
Joel-Venzke <joel.venzke@gmail.com>
2018-01-16 13:09:12 -0700

    add cwt code

---
+++ analysis/wavelet.py
@@ -0,0 +1,92 @@
+from mpi4py import MPI
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+from matplotlib.colors import LogNorm
+from obspy.signal.tf_misfit import cwt
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+w_0 = 0.057
+ground_state = np.abs(-0.518536)
+min_harm = 4
+max_harm = 14
+
+w_list = range(1, 100)
+w_list += range(100, 1000, 100)
+w_list += list(10**np.arange(3, 5))
+
+v_max_list = 5 * 10.0**np.arange(-3, -4, -1)
+v_min_list = 1 * 10.0**np.arange(-4, -5, -1)
+f_0 = w_0 / 2.0 / np.pi
+f_min = f_0 * min_harm
+f_max = f_0 * max_harm
+plot_pad_time = 0
+
+f = h5py.File("Observables.h5", "r")
+observables = f["Observables"]
+time = observables["time"][1:]
+time_len = time.shape[0]
+dt = time[1] - time[0]
+pulse_envelope = f["Pulse"]["Pulse_envelope_0"][1:]
+pulse_time = f["Pulse"]["time"][1:]
+u_p = (pulse_envelope**2) * 7.2973525664e-3 * 7.2973525664e-3 / 4.0
+state_shift = ground_state + u_p
+
+plot_pad = int(plot_pad_time / dt)
+
+data = observables["dipole_acceleration_0_1"][1:]
+padd2 = 2**np.ceil(np.log2(data.shape[0] * 4))
+data = np.lib.pad(
+    data, (int(np.floor((padd2 - data.shape[0]) / 2)),
+           int(np.ceil((padd2 - data.shape[0]) / 2))),
+    'constant',
+    constant_values=(0.0, 0.0))
+for scale_type in ["lin"]:
+    # for scale_type in ["lin", "log"]:
+    for num in w_list[rank::size]:
+        print "rank", rank, "plotting", num
+        scalogram = cwt(data, dt, num, f_min, f_max, nf=400)
+        lower_idx = scalogram.shape[1] / 2 - (time_len / 2 + plot_pad)
+        upper_idx = scalogram.shape[1] / 2 + (time_len / 2 + plot_pad)
+        scalogram = scalogram[:, lower_idx:upper_idx]
+        time = np.linspace(0, dt * scalogram.shape[1],
+                           scalogram.shape[1]) - plot_pad_time
+        fig = plt.figure()
+        x, y = np.meshgrid(time,
+                           np.logspace(
+                               np.log10(f_min),
+                               np.log10(f_max), scalogram.shape[0]))
+        y /= f_0
+        if scale_type == "lin":
+            plt.contourf(x, y, np.abs(scalogram), 15, cmap='viridis')
+        elif scale_type == "log":
+            min_val = 1e-6
+            max_val = np.abs(scalogram).max()
+            contour_vals = list(10**np.arange(
+                -6.,
+                np.ceil(np.log10(max_val)) + (np.ceil(np.log10(max_val)) + 6.)
+                / 15., (np.ceil(np.log10(max_val)) + 6.) / 15.))
+            plot_data = np.abs(scalogram)
+            plot_data[plot_data < min_val] = min_val
+            plt.contourf(
+                x, y, plot_data, contour_vals, cmap='viridis', norm=LogNorm())
+        else:
+            exit("scale fail")
+        plt.plot(pulse_time, state_shift / w_0, 'r-')
+        plt.plot(pulse_time, (state_shift - 0.125544) / w_0, 'w-')
+        plt.plot(pulse_time, (state_shift - 0.0557645) / w_0, 'w-')
+        plt.plot(pulse_time, (state_shift - 0.031348) / w_0, 'w-')
+        plt.xlabel("Time (a.u)")
+        plt.ylabel("Harmonic Order")
+        plt.yticks(range(1, max_harm + 1, 2))
+        plt.ylim(y.min(), y.max())
+        plt.colorbar()
+        plt.grid(c='gray', ls='--')
+        if scale_type == "lin":
+            plt.savefig(str(num).zfill(6) + "_lin_gird.png")
+        elif scale_type == "log":
+            plt.savefig(str(num).zfill(6) + "_log_gird.png")
+        plt.close()
