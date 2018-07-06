0660cfe03f79633e41fc26d589ff97fea4a3b631
Joel-Venzke <joel.venzke@gmail.com>
2018-07-06 10:54:21 -0600

    debug

--- analysis/e_spec_fold.py
+++ analysis/e_spec_fold.py
@@ -24,8 +24,8 @@ def coulomb_wave_function(l, eta, pho):
     # norm_factor = 2**l * np.exp(-np.pi * eta / 2) * np.abs(
     #     gamma(l + 1 + 1.0j * eta)) / factorial(2 * l + 1)
     product = np.arange(1.0, l + 1.0)
+    print "prod", product
     product = eta**2 + product**2
-    print product
     product = np.prod(product)
     norm_factor = 2**l * np.sqrt(2 * np.pi * eta / (
         np.exp(np.pi * eta / 2) - 1) * product) / factorial(2 * l + 1)
