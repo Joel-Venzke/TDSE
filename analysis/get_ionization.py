825701f49bfd0f99d8106cf27e06f3c7203beec5
Joel Venzke <joel.a.venzke@gmail.com>
2019-04-23 14:43:30 -0600

    clean up ionization code

--- analysis/get_ionization.py
+++ analysis/get_ionization.py
@@ -1,29 +1,5 @@
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import h5py
-from scipy.signal import argrelmin, argrelmax
-from matplotlib.colors import LogNorm
-
-
-# return list of states up to state_number
-def state_name(state_number, l_max, m_max):
-
-    name_list = []
-    state_idx = 0
-    n_val = 1
-    while state_idx < state_number:
-        for l_val in np.arange(0, min(n_val, l_max + 1)):
-            m_range = min(l_val, m_max)
-            for m_val in np.arange(-m_range, m_range + 1):
-                name_list.append("(" + str(n_val) + "," + str(l_val) + "," +
-                                 str(m_val) + ")")
-                state_idx += 1
-        n_val += 1
-
-    return name_list
-
 
 f = None
 p = None
